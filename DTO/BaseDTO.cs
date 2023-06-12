edf9acea924f74b722de0746711501f363df65d5
Mostaq Hossain <mhossain@usxpress.com>
2023-06-12 12:21:55 -0400

    Base DTO aded, Driver DTO modified and infrastructure yaml added

---
+++ DTO/BaseDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace FleetForceAPI.DTO
+{
+    public class BaseDTO
+    {
+        /// <summary>
+        /// TraceId for tracking the API request
+        /// </summary>
+        public string TraceId { get; set; }
+
+        /// <summary>
+        /// Track the Username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Track the Application Name
+        /// </summary>
+        public string ApplicationName { get; set; }
+    }
+}
--- DTO/DriverDTO.cs
+++ DTO/DriverDTO.cs
@@ -4,19 +4,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FleetForceAPI.DTO
 {
-    public class DriverDTO
+    public class DriverDTO : BaseDTO 
     {
-        [BsonRepresentation(BsonType.String)]
-        [BsonElement("id")]
         public string Id { get; set; }
-        [BsonElement("name")]
-        //[Required]
+
         public string Name { get; set; }
-        [BsonElement("loadDate")]
+       
         public DateTime LoadDate { get; set; }
-        [BsonElement("customer")]
+        
         public string Customer { get; set; }
-        [BsonElement("load")]
+
         public int Load { get; set; }
     }
 }
