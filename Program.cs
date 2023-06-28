baee760866a993883b906760edcd2db3ac9057d7
Mostaq Hossain <mhossain@usxpress.com>
2023-06-28 17:10:59 -0400

    Added try catch block in Program.cs class

--- Program.cs
+++ Program.cs
@@ -9,49 +9,58 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add connection string
-builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings"));
-
-// Register mongoDb configuration as a singleton object
-builder.Services.AddSingleton<IMongoDatabase>(options =>{
-    var settings = builder.Configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
-    var client = new MongoClient(settings.ConnectionString);
-    return client.GetDatabase(settings.DatabaseName);
-});
-
-// Register driver repository
-builder.Services.AddSingleton<IDriverRepository, DriverRepository>();
-
-// Register truck repository
-builder.Services.AddSingleton<ITruckRepository, TruckRepository>();
-
-// Register load repository
-builder.Services.AddSingleton<ILoadRepository, LoadRepository>();
-
-// Register autoMapper
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Add connection string
+    builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings"));
+
+	// Register mongoDb configuration as a singleton object
+	builder.Services.AddSingleton<IMongoDatabase>(options =>
+	{
+		var settings = builder.Configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+		var client = new MongoClient(settings.ConnectionString);
+		return client.GetDatabase(settings.DatabaseName);
+	});
+
+	// Register driver repository
+	builder.Services.AddSingleton<IDriverRepository, DriverRepository>();
+
+	// Register truck repository
+	builder.Services.AddSingleton<ITruckRepository, TruckRepository>();
+
+	// Register load repository
+	builder.Services.AddSingleton<ILoadRepository, LoadRepository>();
+
+	// Register autoMapper
+	builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+	var app = builder.Build();
+
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseCors(builder => builder
+	 //.AllowAnyOrigin()
+	 .WithOrigins("http://localhost:27017", "http://localhost:49153")
+	 .AllowAnyOrigin()
+	 .AllowAnyMethod()
+	 .AllowAnyHeader()
+	);
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
+
 }
-
-app.UseCors(builder => builder
- //.AllowAnyOrigin()
- .WithOrigins("http://localhost:27017", "http://localhost:49153")
- .AllowAnyOrigin()
- .AllowAnyMethod()
- .AllowAnyHeader()
-);
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
